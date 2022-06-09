b750a640664a98367c8a7d0365a4300607ab4af9
Zaki Shaikh <zaki-shaikh@outlook.com>
2022-06-09 23:25:39 +0530

    Added Express.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello Express')
+const express = require('express')
+
+const PORT = 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Hello from Express</h1>`)
+})
+app.post('/', (req, res) => {
+    res.send(`<h1>Hello from Express (POST)</h1>`)
+})
+app.put('/', (req, res) => {
+    res.send(`<h1>Hello from Express (PUT)</h1>`)
+})
+app.delete('/', (req, res) => {
+    res.send(`<h1>Hello from Express (DELETE)</h1>`)
+})
+
+app.listen(PORT, () => console.log(`Server started running on ${PORT}`))
